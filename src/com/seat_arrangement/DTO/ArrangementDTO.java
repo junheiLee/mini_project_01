ee178efe247288afbe91487552afab8467cbaa6a
junheiLee <jun2ee22@naver.com>
2023-07-04 21:25:01 +0900

    feat: Model getter setter 추가

--- src/com/seat_arrangement/DTO/ArrangementDTO.java
+++ src/com/seat_arrangement/DTO/ArrangementDTO.java
@@ -8,4 +8,35 @@ public class ArrangementDTO {
     private int seatId;
     private int studentId;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getSeatId() {
+        return seatId;
+    }
+
+    public void setSeatId(int seatId) {
+        this.seatId = seatId;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
 }
--- src/com/seat_arrangement/DTO/SeatDTO.java
+++ src/com/seat_arrangement/DTO/SeatDTO.java
@@ -6,4 +6,35 @@ public class SeatDTO {
     private int row;
     private boolean isUsed;
 
+    public int getSeatId() {
+        return seatId;
+    }
+
+    public void setSeatId(int seatId) {
+        this.seatId = seatId;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public boolean isUsed() {
+        return isUsed;
+    }
+
+    public void setUsed(boolean used) {
+        isUsed = used;
+    }
 }
